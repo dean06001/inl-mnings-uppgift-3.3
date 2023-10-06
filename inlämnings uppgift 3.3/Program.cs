0aea2a5c05e72f576e099ac80f554d575201ad84
dean06001 <dean06001@E-PW0234KC>
2023-10-06 13:12:52 +0200

    Add project files.

---
+++ inlämnings uppgift 3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace inlämnings_uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej ur länge vill du hyra ett fordon?");
+            int timmar = int.Parse(Console.ReadLine());
+            if (timmar * 80 > 950)
+            {
+                Console.WriteLine("Det kommer att kosta 950");
+            }
+            else
+            {
+                int hyra = timmar * 80;
+                Console.WriteLine("Det kommer att kosta dig " + hyra + "kr.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
